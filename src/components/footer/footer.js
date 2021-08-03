2b6714bd80b7ca805cc6d5e85774334950ee0f6b
Traci <traci.face@gmail.com>
2021-08-02 22:37:56 -0400

    first commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="fixed-bottom d-flex justify-content-between flex-md-row">
+            <p className="text-secondary"></p>
+
+        </footer>
+    )
+}
+ 
+export default Footer;
